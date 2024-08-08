5272de699fbd444d40ef5127c63e3789f8816d2a
regencul <k.leongardt.95@gmail.com>
2024-08-09 01:56:58 +0300

    Create SecureStorage.kt

---
+++ app/src/main/java/com/example/fickbookauthorhelper/logic/SecureStorage.kt
@@ -0,0 +1,77 @@
+package com.example.fickbookauthorhelper.logic
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Inject
+import javax.inject.Singleton
+
+interface ISecureStorageSaver {
+    fun saveUsername(username: String)
+    fun savePassword(password: String)
+}
+
+interface ISecureStorageProvider {
+    fun getUsername(): String?
+    fun getPassword(): String?
+}
+
+class SecureStorage @Inject constructor(@ApplicationContext applicationContext: Context) : ISecureStorageSaver,
+    ISecureStorageProvider {
+    private val sharedPreferences: SharedPreferences
+
+    init {
+        val masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+        sharedPreferences = EncryptedSharedPreferences.create(
+            "secure_prefs",
+            masterKeyAlias,
+            applicationContext,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+    }
+
+    override fun saveUsername(username: String) {
+        sharedPreferences.edit().putString("username", username).apply()
+    }
+
+    override fun savePassword(password: String) {
+        sharedPreferences.edit().putString("password", password).apply()
+    }
+
+    override fun getUsername(): String? {
+        return sharedPreferences.getString("username", null)
+    }
+
+    override fun getPassword(): String? {
+        return sharedPreferences.getString("password", null)
+    }
+
+    companion object {
+        private const val PREFS_NAME = "FickbookUserEncryptedPrefs"
+    }
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object SecureStorageModule {
+    @Provides
+    @Singleton
+    fun provideSecureStorage(@ApplicationContext context: Context): SecureStorage {
+        return SecureStorage(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideISecureStorageSaver(store: SecureStorage): ISecureStorageSaver = store
+
+    @Provides
+    @Singleton
+    fun provideISecureStorageProvider(store: SecureStorage): ISecureStorageProvider = store
+}
